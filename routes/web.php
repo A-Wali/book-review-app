d104a07b8abf67aef55196b118cc414d6eb67d2b
Abdul Wali <waliurrehman185@gmail.com>
2025-02-18 23:42:53 +0500

    routes

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,7 @@ Route::get('/', function () {
 Route::get('account/register',[AccountController::class,'register'])->name('account/register');
 Route::post('account/register',[AccountController::class,'processregister'])->name('account/processregister');
 Route::get('account/login',[AccountController::class,'login'])->name('account/login');
+Route::get('account/login',[AccountController::class,'login'])->name('account/login');
+
+Route::get('account/login',[AccountController::class,'login'])->name('account/login');
+
